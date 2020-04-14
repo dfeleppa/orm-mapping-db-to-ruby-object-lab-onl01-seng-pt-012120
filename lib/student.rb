8eba0911ae2e324dc59d3fdffababa3844ca54f2
Daniel J Feleppa <dfeleppa@gmail.com>
2020-04-14 12:38:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,11 +88,19 @@ class Student
   end
   
   def self.first_student_in_grade_10
-  
+  sql = "SELECT * 
+      FROM students 
+      WHERE grade = 10
+      LIMIT 1;"
+      DB[:conn].execute(sql)
   end
   
   def self.all_students_in_grade_X(grade)
-  
+      sql = "SELECT * 
+      FROM students 
+      WHERE grade = ?;"
+    
+    DB[:conn].execute(sql)
   end
   
 end
